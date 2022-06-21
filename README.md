a9102690967b08e469cafbf0c53fc298cdf4564b
117503445 <t117503445@gmail.com>
2022-06-21 14:49:27 +0000

    feat: init gf

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # mall-gf-dapr
+
 microservice mall based on goframe and dapr
+
+```sh
+docker-compose -f docker-compose-dev.yml up -d
+
+```
---
+++ src/app/api-template/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	_ "mall-gf-dapr/app/api-template/internal/packed"
+
+	"github.com/gogf/gf/v2/os/gctx"
+
+	"mall-gf-dapr/app/api-template/internal/cmd"
+)
+
+func main() {
+	cmd.Main.Run(gctx.New())
+}
