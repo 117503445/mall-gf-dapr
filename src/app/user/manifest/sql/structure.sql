c5927721966cf2a3d3f5b404e86a86ac9d912c49
117503445 <t117503445@gmail.com>
2022-06-22 05:12:52 +0000

    feat: config and sql

---
+++ src/app/user/manifest/sql/structure.sql
@@ -0,0 +1,16 @@
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE `user` (
+    `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+    `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+    `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    `deleted_at` datetime(3) NULL DEFAULT NULL,
+
+    `name` varchar(255) NOT NULL DEFAULT '' COMMENT '用户姓名',
+    `gender` tinyint(3) unsigned NOT NULL DEFAULT '0' COMMENT '用户性别',
+    `mobile` varchar(255) NOT NULL DEFAULT '' COMMENT '用户电话',
+    `password` varchar(255) NOT NULL DEFAULT '' COMMENT '用户密码',
+    
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `idx_mobile_unique` (`mobile`)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
