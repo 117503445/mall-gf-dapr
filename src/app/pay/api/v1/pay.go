13e44accd998e79b664e5c2d9d193c75126250c2
117503445 <t117503445@gmail.com>
2022-07-14 17:49:52 +0000

    feat: pay api

---
+++ src/app/pay/api/v1/pay.go
@@ -0,0 +1,60 @@
+package v1
+
+import (
+	"117503445/mall-gf-dapr/app/utility"
+
+	"github.com/gogf/gf/v2/frame/g"
+)
+
+type CreateReq struct {
+	g.Meta  `path:"/" tags:"pay" method:"post"`
+	OrderID uint64
+}
+
+type CreateRes struct {
+	g.Meta   `mime:"application/json"`
+	MetaInfo utility.RspMetaInfo `json:"-"`
+	Id       int
+	Status   int
+}
+
+type GetReq struct {
+	g.Meta `path:"/:id" tags:"pay" method:"get"`
+	Id     int
+}
+
+type GetRes struct {
+	g.Meta   `mime:"application/json"`
+	MetaInfo utility.RspMetaInfo `json:"-"`
+	Id       int
+	Status   int
+}
+
+type CallbackReq struct {
+	g.Meta `path:"/callback/:id" tags:"pay" method:"post"`
+
+	Id     int
+	Amount int
+
+	Source string
+	Sign   string
+}
+
+type CallbackRes struct {
+	g.Meta   `mime:"application/json"`
+	MetaInfo utility.RspMetaInfo `json:"-"`
+}
+
+
+type CancelReq struct {
+	g.Meta `path:"/cancel/:id" tags:"pay" method:"post"`
+
+	Id     int
+}
+
+type CancelRes struct {
+	g.Meta   `mime:"application/json"`
+	MetaInfo utility.RspMetaInfo `json:"-"`
+
+	Status int
+}
